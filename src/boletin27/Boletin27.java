6a03f8516721236c6dbaf5a674890558673415ff
palonsovazquez <pav.vigo@gmail.com>
2019-06-03 11:48:30 +0200

    Fin

---
+++ src/boletin27/Boletin27.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin27;
+
+/**
+ *
+ * @author Pablo Alonso Vazquez <pav.vigo@gmail.com>
+ */
+public class Boletin27 {
+
+
+    public static void main(String[] args) {
+        new Calculadora().setVisible(true);
+    }
+    
+}
